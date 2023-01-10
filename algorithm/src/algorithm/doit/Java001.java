9d5d06e648516ed15ee32fbee61f1a5a0c0d81aa
김형빈\형빈 <wmks2719@gmail.com>
2023-01-10 14:22:02 +0900

    Do it 알고리즘 코딩테스트 2번문제

--- algorithm/src/algorithm/doit/Java001.java
+++ algorithm/src/algorithm/doit/Java001.java
@@ -1,10 +1,24 @@
 package algorithm.doit;
 
+import java.util.Scanner;
+
 public class Java001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		String sNum = sc.next();
+		char[] cNum = sNum.toCharArray();
+		
+		int sum = 0;
+		for(int i = 0 ; i < cNum.length ; i++) {
+			sum += cNum[i] - '0';
+		}
+		System.out.println(sum);
 	}
 
 }
